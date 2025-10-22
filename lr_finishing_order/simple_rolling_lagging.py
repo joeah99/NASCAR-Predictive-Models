7c8a58df9e31f56dbcf50dd7ebc940ce54674069
Joseph Holy <joeah99@gmail.com>
2025-10-21 22:39:26 -0500

    Reverted rolling_lagging.py to a previous version and included points_position in training, improving prediction accuracy by around 0.14 positions on average. Also added run instructions and improved UX.

---
+++ lr_finishing_order/simple_rolling_lagging.py
@@ -0,0 +1,48 @@
+import pandas as pd
+
+def lagging_rolling_generator(df, features, sort_list, filter_list, window, suffix):
+    new_cols = {}
+    sort_list.append("race_date")
+    # sort by driver / car / team then by race_date in a copy of the dataframe
+    df = df.sort_values(sort_list).copy()  
+    
+    for feature in features:
+        # create lag1 features (previous race)
+        new_cols[f"{feature}_lag1_{suffix}"] = (
+            df.groupby(filter_list)[feature].shift(1)
+        )
+        # create rolling mean features (avg over previous w races)
+        new_cols[f"{feature}_roll{window}_{suffix}"] = (
+            df.groupby(filter_list)[feature]
+            # shift(1) moves back a race to prevent leakage, min_periods=n to ensure at least n prior races to compute a meaningful average
+            .transform(lambda x: x.shift(1).rolling(window, min_periods=1).mean())
+        )
+
+    df = pd.concat([df, pd.DataFrame(new_cols, index=df.index)], axis=1)
+    return df
+    
+
+# for benchmark model only (for now) replaces null driver lag/rolling features with car/team lag/rolling features
+# since I don't have to 'train' with this, don't need to worry about every team being 'new' at beginning of dataset
+# still working on solution in case carteam is new as well, probably do manually for now
+
+def reconcile_driver_carteams(df, features, window, suffix):
+    
+    for feature in features:
+        # fill lagging features
+        driver_lag_col = f"{feature}_lag1_{suffix}"
+        team_lag_col = f"{driver_lag_col}_team"
+        carteam_lag_col = f"{driver_lag_col}_carteam"
+
+        df[carteam_lag_col] = df[carteam_lag_col].fillna(df[team_lag_col])
+        df[driver_lag_col] = df[driver_lag_col].fillna(df[carteam_lag_col])
+        
+        # fill rolling features
+        driver_roll_col = f"{feature}_roll{window}_{suffix}"
+        team_roll_col = f"{driver_roll_col}_team"
+        carteam_roll_col = f"{driver_roll_col}_carteam"
+            
+        df[carteam_roll_col] = df[carteam_roll_col].fillna(df[team_roll_col])
+        df[driver_roll_col] = df[driver_roll_col].fillna(df[carteam_roll_col])
+    
+    return df
